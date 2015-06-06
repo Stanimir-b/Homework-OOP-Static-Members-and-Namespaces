27f666c7b3da8ad8a16e73b1104304573c615008
Stanimir-b <stanimir.b@gmail.com>
2015-06-06 09:29:10 +0300

    Create Path3D.cs

---
+++ Paths/Path3D.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Point;
+
+ public class Path3D
+{
+    private   List<Point3D> path = new List<Point3D>();
+    public  List<Point3D> Path { get
+    {
+        return this.path;
+    }
+        set
+        {
+            this.path = value;
+        }
+    }
+    
+    public Path3D(List<Point3D> path)
+    {
+        this.Path = path;
+    }
+    public override string ToString()
+    {
+        StringBuilder result = new StringBuilder();
+        foreach (var points in this.Path)
+        {
+            result.AppendLine("X="+points.X+" "+"Y="+points.Y+" "+"Z="+points.Z);
+        }
+        return result.ToString();
+    }
+     
+}
+
